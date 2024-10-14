1cf47387c3321163b601244e33cf0c61f1d143b8
Gabriel Akbarov <kontakt@gabrielakbarov.de>
2024-10-14 13:27:49 +0200

    A

--- Uebungsaufgaben/02_Schleifen-und-Bedingungen/A.c
+++ Uebungsaufgaben/02_Schleifen-und-Bedingungen/A.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    long int n = 1;
+    long double e = 1;
+    int lim = 10;
+
+    for (int i = 1; i <= lim; i++) {
+        n *= i;
+        e += (1.0L / n);
+    }
+
+    printf("e = %Lf", e);
     return 0;
 }
