25530c3b5b962d4cd644d7b6f6350d06bfb2b17a
Gabriel Akbarov <kontakt@gabrielakbarov.de>
2024-10-16 14:26:18 +0200

    Erweiterte Version von B und C (als Funktion)

--- Uebungsaufgaben/02_Schleifen-und-Bedingungen/B2.c
+++ Uebungsaufgaben/02_Schleifen-und-Bedingungen/B2.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
+void output(int n) {
+    for (int i = 0; i < n; i++) {
+        int limit = i <= n / 2 ? i : n - i - 1;
+
+        for (int j = 0; j < limit; j++) {
+            printf("+");
+        }
+
+        printf(i <= n / 2 ? "A" : "B");
+
+        for (int j = 0; j < n - 2 * limit - 2; j++) {
+            printf("+");
+        }
+
+        if (i != n / 2) {
+            printf(i <= n / 2 ? "B" : "A");
+        }
+
+        for (int j = 0; j < limit; j++) {
+            printf("+");
+        }
+
+        printf("\n");
+    }
+}
 
 int main(void) {
     int n;
     printf("Bitte gib eine Zahl ein:");
     scanf(" %i", &n);
 
-    for (int i = 0; i < n; i++) {
-        int limit = i <= n / 2 ? i : n - i - 1;
-        for (int j = 0; j < limit; j++) {
-            printf("+");
-        }
-        printf(i <= n / 2 ? "A" : "B");
-        for (int j = 0; j < n - 2 * limit - 2; j++) {
-            printf("+");
-        }
-        if (i != n / 2) {
-            printf(i <= n / 2 ? "B" : "A");
-        }
-        for (int j = 0; j < limit; j++) {
-            printf("+");
-        }
-        printf("\n");
-    }
+    output(n);
+
     return 0;
 }
---
+++ Uebungsaufgaben/02_Schleifen-und-Bedingungen/C2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void output(int n) {
+    int m = 0;
+
+    for (int i = 1; i <= n*2-1; i++) {
+        if (i <= n) {
+            m++;
+        } else {
+            m--;
+        }
+
+        for (int j = 1; j <= n - m; j++) {
+            printf(" ");
+        }
+
+        for (int k = 1; k <= m * 2 - 1; k++) {
+            printf("+");
+        }
+
+        for (int j = 1; j <= n - m; j++) {
+            printf(" ");
+        }
+
+        printf("\n");
+    }
+}
+
+int main(void) {
+    int n;
+    printf("Bitte gib eine Zahl zwischen 1 und 10 ein:");
+    scanf(" %i", &n);
+
+    output(n);
+
+    return 0;
+}
